a0b58836de617cb1305138763a29d12c5f0c0459
4015b15 <4015b15@melon.ece.uprm.edu>
2017-02-08 11:07:32 -0400

    Bandera v2.0

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
+		JFrame myFrame = new JFrame("Rolando Tremont");
 		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(700, 260);
 		myFrame.setSize(500, 250);
