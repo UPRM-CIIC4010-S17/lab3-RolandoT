c89e65a2443dfd7dc3598b5c94801ec1fdd3760c
4015b15 <4015b15@melon.ece.uprm.edu>
2017-02-08 09:56:13 -0400

    Lab3 panel

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,8 +4,10 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Hello, world!");
 		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		myFrame.setLocation(700, 260);
+		myFrame.setSize(400, 400);
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 	}
 }
